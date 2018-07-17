06d6845c780cecc73428735dcfdc1791a0040e27
sk <semaria.eyzac@gmail.com>
2018-07-17 13:36:25 -0700

    install bootstrap and favicon and fix the welcome title in the app component

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component{
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Senior Care App</h1>
         </header>
         <Switch>
           <Route exact path='/' component={ClientList} />
@@ -27,7 +27,7 @@ class App extends React.Component{
         </div>
       </Fragment>
       </BrowserRouter>
-  
+
   }
 
 };
