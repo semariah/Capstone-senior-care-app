d5e2f957014f5f1919125ff47095fc719f63a8a1
sk <semaria.eyzac@gmail.com>
2018-07-20 12:23:39 -0700

    remove the navbar component for now and fix the routes for home and clientlist to be rendered on app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React, { Fragment, Component} from 'react'
+import React, { Fragment, Component } from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Error404 from './error404/Error404'
-//import Home from './home/Home'
-import header from './layouts/Navbar'
+import Home from './home/Home'
+//import header from './layouts/Navbar'
 import ClientControl from './client/ClientControl'
 import ClientList from './client/ClientList'
 import logo from '../logo.svg'
@@ -26,18 +26,19 @@ class App extends React.Component{
   }
 
   render(){
-    const { header } = this.state
-    const { Navbar } = this.state
+    //const { header } = this.state
+    //const { Navbar } = this.state
     return <BrowserRouter>
     <Fragment>
         <div className="App">
-        <header />
-        // <header className="App-header">
-        //   <img src={logo} className="App-logo" alt="logo" />
-        //   <h1 className="App-title">Welcome to Senior Care App</h1>
-        // </header>
+        //<header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Senior Care App</h1>
+        </header>
+        <Home/>
         <Switch>
-          <Route exact path='/clientlist' render={()=><ClientList clientList={this.state.masterClientList} />} />
+          <Route exact path='/' render={()=><ClientList clientList={this.state.masterClientList} />} />
           <Route path='/newclient' render={()=><ClientControl onClientCreation={this.handleAddingClientToList} />} />
           <Route component={Error404} />
         </Switch>
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown, Jumbotron, Button } from 'react-bootsrap'
-
-// function Navbar(){
-//   return(
-//     <div>
-//       <ul>
-//         <Link to ='/'><li>Home</li></Link>
-//         <Link to ='/ClientForm'><li>Client Form</li></Link>
-//         <Link to ='/ClientList'><li>Client</li></Link>
-//         <Link to ='/Admin'><li>LogIn</li></Link>
-//       </ul>
-//     </div>
-//   )
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import { Navbar, Nav, NavItem, MenuItem, NavDropdown, Jumbotron, Button } from 'react-bootsrap'
+//
+// // function Navbar(){
+// //   return(
+// //     <div>
+// //       <ul>
+// //         <Link to ='/'><li>Home</li></Link>
+// //         <Link to ='/ClientForm'><li>Client Form</li></Link>
+// //         <Link to ='/ClientList'><li>Client</li></Link>
+// //         <Link to ='/Admin'><li>LogIn</li></Link>
+// //       </ul>
+// //     </div>
+// //   )
+// // }
+// // export default Navbar
+//
+// class header extends React.Component {
+//   render() {
+//     return(
+//       <div className="Nav_container">
+//       <Navbar inverse collapseOnSelect>
+//   <Navbar.Header>
+//     <Navbar.Brand>
+//       <a href="#brand">Home</a>
+//     </Navbar.Brand>
+//     <Navbar.Toggle />
+//   </Navbar.Header>
+//   <Navbar.Collapse>
+//     <Nav>
+//       <NavItem eventKey={1} href="#">
+//         About Us
+//       </NavItem>
+//       <NavItem eventKey={2} href="#">
+//         Task
+//       </NavItem>
+//       <NavItem eventKey={3} href="#">
+//         Employee
+//       </NavItem>
+//       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+//         <MenuItem eventKey={3.1}>Client-Form</MenuItem>
+//         <MenuItem eventKey={3.2}>Client-List</MenuItem>
+//       </NavDropdown>
+//     </Nav>
+//     <Nav pullRight>
+//       <NavItem eventKey={1} href="#">
+//         LogIn
+//       </NavItem>
+//     </Nav>
+//   </Navbar.Collapse>
+// </Navbar>
+// </div>
+//     )
+//   }
 // }
-// export default Navbar
-
-class header extends React.Component {
-  render() {
-    return(
-      <div className="Nav_container">
-      <Navbar inverse collapseOnSelect>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#brand">Home</a>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} href="#">
-        About Us
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Task
-      </NavItem>
-      <NavItem eventKey={3} href="#">
-        Employee
-      </NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Client-Form</MenuItem>
-        <MenuItem eventKey={3.2}>Client-List</MenuItem>
-      </NavDropdown>
-    </Nav>
-    <Nav pullRight>
-      <NavItem eventKey={1} href="#">
-        LogIn
-      </NavItem>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-</div>
-    )
-  }
-}
-
-export default header
+//
+// export default header
