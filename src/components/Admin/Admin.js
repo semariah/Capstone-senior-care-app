08075718645b8a889893155af4174297d7a90490
sk <semaria.eyzac@gmail.com>
2018-07-23 10:58:05 -0700

    reuse of the clientList component in the admin component  to list all the clients available

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ClientList from '../client/ClientList'
 
 function Admin(props){
   return(
     <div>
       <h2>Admin</h2>
+      <ClientList clientList={props.clientList} />
     </div>
   );
 }
